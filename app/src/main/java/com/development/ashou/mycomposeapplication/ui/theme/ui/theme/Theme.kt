17ba67fa5c78d793672b89d0920debab3b602bfd
Zakaraya Ashour <ashourz@gmail.com>
2022-02-06 09:16:31 -0600

    Completion of Jetpack Compose Tutorial - Using State in Jetpack Compose https://developer.android.com/codelabs/jetpack-compose-state

--- app/src/main/java/com/development/ashou/mycomposeapplication/ui/theme/ui/theme/Theme.kt
+++ app/src/main/java/com/development/ashou/mycomposeapplication/ui/theme/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun MyComposeApplicationTheme(
+fun JetpackComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
